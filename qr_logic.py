c36692c953213e22d61dbae5816d16d39d06260d
William De Leon <willyorlando10@gmail.com>
2026-01-28 17:05:17 -0800

    updated the url to a user input for dynamic urls

--- qr_logic.py
+++ qr_logic.py
@@ -4,10 +4,14 @@ def generate_my_qr(data, file_name):
     # --- STEP 1: Configure the Generator (Library Logic) ---
     qr = qrcode.QRCode(
         version=1, # Controls the size (1 is smallest)
-        error_correction=qrcode.constants.ERROR_CORRECT_L, # About 7% damage recovery
+
+        #error_correction=qrcode.constants.ERROR_CORRECT_L is used when you dont want a logo in the middle
+        error_correction=qrcode.constants.ERROR_CORRECT_H, # About 7% damage recovery
         box_size=10, # How many pixels each "dot" is
         border=4,    # Thickness of the white border
+    
     )
+    
 
     # --- STEP 2: Feed the data ---
     qr.add_data(data)
@@ -22,5 +26,5 @@ def generate_my_qr(data, file_name):
 
 # --- EXECUTION ---
 # This is where you decide WHAT goes into the code
-user_input = "https://www.google.com" 
+user_input = input("Enter the url you would like to encode: ")
 generate_my_qr(user_input, "my_first_qr.png")
